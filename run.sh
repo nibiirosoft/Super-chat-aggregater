f0aecfcf6c108ee7e3e993738390c67e0e3f14d1
nibiironokane <65806595+nibiirosoft@users.noreply.github.com>
2020-05-31 03:08:07 +0900

    Add files via upload

--- run.sh
+++ run.sh
@@ -43,6 +43,66 @@ while true; do
   fi
 done
 
+while true; do
+  videoIds=$(echo -e "$data" | grep 'watch?v=' | sed -e 's/^.*watch?v=\([^"]*\)".*$/\1/' | sed -e 's/\\//' | awk '!a[$0]++')
+  echo -e "$videoIds" >> $RESULT_DIR/watch.list.txt
+
+  n=$(echo -e "$videoIds" | wc -l)
+  videoIds_n=$((videoIds_n + n))
+
+  continuation=$(echo -e "$data" | grep 'continuation=' | sed -e 's/^.*;continuation=\([^"&]*\)[&"].*$/\1/' | sed -e 's/\\//')
+
+  if [ "$continuation" == "" ]; then
+    break
+  fi
+
+  while true; do
+    echo "["`date +'%Y/%m/%d %H:%M:%S'`"]" "[wget] videos #$videoIds_n"
+    data=$(wget --no-check-certificate --no-cache --no-cookies --content-on-error=on "https://www.youtube.com/browse_ajax?continuation=$continuation" -qO -)
+
+    if echo -e "$data" | head -n 1 | grep -v reload >/dev/null; then
+      break
+    fi
+  done
+done
+
+i=0
+n=$(cat $RESULT_DIR/watch.list.txt | wc -l)
+cat $RESULT_DIR/watch.list.txt | while read id; do
+  i=$((i+1))
+  url="https://www.youtube.com/watch?v=$id"
+
+  echo "["`date +'%Y/%m/%d %H:%M:%S'`"]" "[get_super_chat.py] (#$i/$n) videoId=$id"
+  $SCRIPT_DIR/bin/get_super_chat.py "$url" | sed 's/\xc2\xa0//g' | sed 's/\xef\xbf\xa5/\\/g' | sed 's/\xc2\xa3/GBP/g' | sed 's/\xe2\x82\xa9/KPW/g' | sed 's/\xe2\x82\xb9/INR/g' | sed 's/\xe2\x82\xac/EUR/g' | tee -a $RESULT_DIR/purchase.list.txt | awk -F '\t' 'BEGIN{OFS="\t";
+    while(getline < "'$SCRIPT_DIR/config.txt'") {
+      if ($1!=""){
+        xe[$1] = $2;
+      }
+    }
+  }{
+    id = $1;
+    date = $2;
+    name = $3;
+    c = $4;
+    amount = $5;
+    comment = $6;
+    add = 0;
+    if(xe[c] > 0){
+      add = amount * xe[c];
+    }
+    else{
+      add = 0;
+      print c, amount >> "'$RESULT_DIR/purchase.summary.other.txt'";
+    }
+    total += int(add);
+    print id, date, name, add, comment;
+  }END{
+    if(total > 0){
+      print id, date, total >> "'$RESULT_DIR/purchase.summary.video.txt'"
+    }
+  }' >> $RESULT_DIR/purchase.list2.txt
+done
+
 
 echo '----'
 cat $RESULT_DIR/purchase.summary.video.txt | sort -t $'\t' -k 3rn | head -n 5
@@ -96,3 +156,29 @@ cat $RESULT_DIR/purchase.summary.name.txt | head -n 5
 
 echo '----'
 cat $RESULT_DIR/purchase.summary.other.txt 2>/dev/null
+
+rm -f $RESULT_DIR/purchase.summary.name.*.txt
+cat $RESULT_DIR/purchase.summary.name.txt | head -n 5 | awk -F '\t' '{OFS="\t";}{
+  name = $1;
+
+  file = "'$RESULT_DIR/purchase.list2.txt'";
+  while(getline < file) {
+    if(oldId!="" && oldId!=$1){
+      print oldId, oldDate, name, pay, int(pay/(pay+other)*1000)/10"%" >> "'$RESULT_DIR/purchase.summary.name.'"name".txt"
+      pay = 0;
+      other = 0;
+    }
+    if($3==name){
+      pay += $4;
+    }
+    else{
+      other += $4;
+    }
+
+    oldId = $1;
+    oldDate = $2;
+  }
+  close(file);
+
+  print oldId, oldDate, name, pay, int(pay/(pay+other)*1000)/10"%" >> "'$RESULT_DIR/purchase.summary.name.'"name".txt"
+}'
